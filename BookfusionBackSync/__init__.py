5f0a3ece8273aa718ec76c48f6672b4ced438d64
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2026-04-26 15:26:32 +0600

    обновим метаданные на реальные

--- BookfusionBackSync/__init__.py
+++ BookfusionBackSync/__init__.py
@@ -7,8 +7,8 @@ class BookFusionBackSyncPlugin(InterfaceActionBase):
     name = 'BookFusion Back Sync'
     description = 'Sync last-read dates from BookFusion back into Calibre custom columns.'
     supported_platforms = ['windows', 'osx', 'linux']
-    author = 'BookFusion'
-    version = (0, 1, 0)
+    author = 'Dmitriy Kazimirov'
+    version = (0, 0, 2)
     minimum_calibre_version = (6, 2, 1)
 
     actual_plugin = 'calibre_plugins.bookfusionbacksync.ui:InterfacePlugin'
